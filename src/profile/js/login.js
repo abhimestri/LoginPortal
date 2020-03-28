11156916bf0cd18116d3875b339e994f3721af0c
Abhishek Mesti <amestri890@gmail.com>
2020-03-28 13:23:46 +0530

    responsivness
    add a max level of responsivness to all the parts of the web-page

--- src/profile/js/login.js
+++ src/profile/js/login.js
@@ -1,3 +1,6 @@
+
+// this part of code contains toggleing display of signin and signup block
+
 document.querySelector("#sign-up-btn").addEventListener('click',signUpBtn);
 function signUpBtn(){
     document.querySelector(".sub-main-section-content").style.display = "none";
@@ -10,6 +13,10 @@ function loginBackBtn(){
     document.querySelector(".hidden-sign-up-section").style.display = "none";
     document.querySelector(".floating-content-in-header").style.display = "block";
 }
+
+// SIGN-IN PART 
+// this part of code contains displaying the input border style animation  
+
 document.querySelector('#mail-id-input').addEventListener('click',mailidinputresponse);
 function mailidinputresponse(){
     document.getElementById('mail-id-input').style.borderColor = "blue";
@@ -22,6 +29,13 @@ function passwordinputresponse(){
     document.getElementById('password-input').style.transition = "0.57s";
     document.getElementById('mail-id-input').style.borderColor = "";
 }
+
+              
+
+
+// SIGN-UP PART 
+// this part of code contains displaying the input border style animation 
+
 document.querySelector('#mail-id-hidden-section-input').addEventListener('click',hiddeninputmail);
 function hiddeninputmail(){
     document.getElementById('mail-id-hidden-section-input').style.borderColor = "blue";
@@ -34,6 +48,12 @@ function hiddeninputpassword(){
     document.getElementById('mobile-number-hidden-section-input').style.transition = "0.57s";
     document.getElementById('mail-id-hidden-section-input').style.borderColor = "";
 }
+
+
+// SIGN-IN part 
+/* this part of code contains animating the input border line to red and displaying the error message 
+     if input fields are left unfilled */    
+
 document.querySelector('#submit-login-btn').addEventListener('click',addingEffects);
 function addingEffects(){
     const mailIdValue = document.getElementById('mail-id-input').value;
@@ -62,7 +82,11 @@ function addingEffects(){
         alert('all good!!');
     }
 }
-// SEE IF U CAN USE DISPLAY-ERROR-MESSAGE INSTEAD OF USIN HIDDEN IN THE BELOW CODE
+
+
+// SIGN-UP PART
+/* this part of code contains animating the input border line to red and displaying the error message 
+     if input fields are left unfilled */  
 
 document.querySelector('#signup-login-btn').addEventListener('click',addingSignUpEffects);
 function addingSignUpEffects(){
@@ -95,10 +119,52 @@ function addingSignUpEffects(){
     }
 }
 
+
+
 //------------------------------------------------------------------//
+
+
+// this is the part of code containing J-QUERY
+/* this part of code will display the forgot password content and will genereate the OTP 
+    if the input phone number is entered or will display the error message if input filled is left empty*/  
+
 document.querySelector('#forgot-password').addEventListener('click',forgotPasswordNewPage);
 function forgotPasswordNewPage(){
     $('.forgot-password-page').show();
-    $('a#forgot-password').attr('target','_blank')
+    $('.body-container').hide();
+    $('#loading-gif-for-OTP').hide();
 }
-// WORK IN PROGRESS//
+
+$('#OTP-generating-btn').click(function(){
+    let mobileNumValue = document.getElementById('forgot-password-mobile-no-input').value;
+    if((mobileNumValue != 0)){
+            $('.forgot-password-page').css('opacity',0.3);
+            $('#loading-gif-for-OTP').css({display:'block',opacity:1});
+            setTimeout(afterLoading,2000);
+            function afterLoading(){
+            {
+               
+            $('#loading-gif-for-OTP').css({display:'none'});
+            }{
+            $('#forgot-password-OTP-message').css('display','block');
+            let OTPnumber = Math.floor((Math.random() * 1000000) + 1);
+            document.getElementById('forgot-display-hidden-error-message').style.display = "none";
+            alert('your one time password is: ' + OTPnumber);
+             $('.forgot-password-page').css('opacity',1);
+            }
+            }
+    }
+    else if(mobileNumValue != 'e'){
+        document.getElementById('forgot-display-hidden-error-message').style.display = "block";
+        $('#forgot-password-OTP-message').css('display','none');
+
+    }
+    else{
+        $('#forgot-password-OTP-message').css('display','none');
+        document.getElementById('forgot-display-hidden-error-message').style.display = "block";
+    }
+})
+$('#forgot-page-to-main-page-btn').click(function(){
+    $('.forgot-password-page').hide();
+    $('.body-container').show();
+})
